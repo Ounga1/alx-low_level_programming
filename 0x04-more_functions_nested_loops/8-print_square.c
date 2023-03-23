79957f4dcb5d76735d880854e124bec199e3cc1a
David Ounga <davidounga07@gmail.com>
2023-03-23 13:17:38 -0700

    Refactor 8-print_square.c

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -1,21 +1,28 @@
-/**
- * print_square - prints a square, followed by a new line
- * @size: size of the square
- */
+#include "main.h"
 
+/**
+ * print_square - a function that prints a square, followed by a new line
+ * @size: size of both width and length
+ * Return: a square made of '#'
+ */
 void print_square(int size)
 {
-    int i, j;
+	int co, ro;
 
-    if (size <= 0) {
-        _putchar('\n');
-        return;
-    }
-
-    for (i = 0; i < size; i++) {
-        for (j = 0; j < size; j++)
-            _putchar('#');
-        _putchar('\n');
-    }
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (co = 1; co <= size; co++)
+		{
+			_putchar('#');
+			for (ro = 2; ro <= size; ro++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
 }
-
