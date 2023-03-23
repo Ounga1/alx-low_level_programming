ed603e2e4d73ef452eddd2bf9a939b281f8321fc
David Ounga <davidounga07@gmail.com>
2023-03-23 07:37:38 -0700

    Add Readme.md

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - main function
+ *
+ * Return: always 0
+ */
+
+int main(void)
+{
+	printf("Size of a char: %lu byte(s)", size of(char));
+	printf("Size of an int: %lu byte(s)", size of(int));
+	printf("Size of a long int: %lu byte(s)", size of(long int));
+	printf("Size of a long long int: %lu byte(s)", size of(long long int));
+	printif("Size of a float: %lu byte(s)", size of(float));
+	return (0);
+}
+
+
