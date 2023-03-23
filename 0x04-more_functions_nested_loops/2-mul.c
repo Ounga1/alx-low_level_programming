8a4555f09db387a03cd902adbbaa6616c9bdd3bf
David Ounga <davidounga07@gmail.com>
2023-03-23 13:04:13 -0700

    Refactoe 2-mul.c

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,13 +1,12 @@
+#include "main.h"
+
+/**
+ *  mul - a function that multiplies two integers
+ * @a: first input
+ * @b: second input
+ * Return: multiplication result of the 2 inputs
+ */
 int mul(int a, int b)
 {
-    int result = 0;
-    while (b != 0)
-    {
-        if (b & 1)
-          result += a;
-        a <<= 1;
-        b >>= 1;
-    }
-    return result;
+	return (a * b);
 }
-
