e1d4426b271aaea231bbaa68b24bc36421682563
David Ounga <davidounga07@gmail.com>
2023-03-23 12:59:17 -0700

    Refactor 1-isdigit.c

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,4 +1,4 @@
-#include "holberton.h"
+#include "main.h"
 
 /**
  * _isdigit -  a function that checks for a digit 0-9
@@ -7,7 +7,7 @@
  */
 int _isdigit(int c)
 {
-	if ((c >= 48) && (c <= 57))
+	if ((c >= '0') && (c <= '9'))
 		return (1);
 	else
 		return (0);
